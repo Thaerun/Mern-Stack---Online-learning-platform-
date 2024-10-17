c0fd876c10f9cd66cd63c91c25bea7e04b58c9e2
Praveen <praveenkal0508@gmail.com>
2024-10-17 10:34:40 +0530

    second commit by praveen

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">MyApp</Navbar.Brand>
+        <Navbar.Brand href="#home">MyApp Navbar example</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
